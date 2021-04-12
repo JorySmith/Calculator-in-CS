5524904736cb368729a3f1a1f8c61192b3b77151
Jory <46983170+Cambro320@users.noreply.github.com>
2021-04-11 20:34:45 -0500

    Added input validation and error catching functionality

--- Program.cs
+++ Program.cs
@@ -2,46 +2,110 @@
 
 namespace Calculator
 {
+    class Calculator
+        // Do the calculations
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // default value is NaN
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d": 
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
     class Program
+        // Do the UI, input requests, input validations, and error capturing
     {
         static void Main(string[] args)
         {
-            int num1 = 0; int num2 = 0;
-            Console.WriteLine("Console Calculator in C#\r");
-            Console.WriteLine("------------------------\n");
+            bool endApp = false;
+            // App title
+            Console.WriteLine("Jory's Magical C# Calculator App\r");
+            Console.WriteLine("--------------------------------\r");
 
-            Console.WriteLine("Type a number, and then press Enter");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Type another number, and then press Enter");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Choose an option below");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Mulitiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option: ");
-
-            switch (Console.ReadLine())
+            while (!endApp)
             {
-                case "a":
-                    Console.WriteLine($"Your result is: {num1} + {num2} = " + (num1 + num2));
-                    break;
-                case "s":
-                    Console.WriteLine($"Your result is: {num1} - {num2} = " + (num1 - num2));
-                    break;
-                case "m":
-                    Console.WriteLine($"Your result is: {num1} * {num2} = " + (num1 * num2));
-                    break;
-                case "d":
-                    Console.WriteLine($"Your result is: {num1} / {num2} = " + (num1 / num2));
-                    break;
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+                // Get input 1 from user
+                Console.Write("Type a number, then press Enter: ");
+                numInput1 = Console.ReadLine();
+
+                // Verify input 1 was a number
+                // while loop testing numInput with double.TryParse, out result assigned to cleanNum var
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("Invalid input. Please enter a whole number: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                // Get input 2 from user
+                Console.Write("Type a second number, then press Enter: ");
+                numInput2 = Console.ReadLine();
+
+                // Verify input 2 was a number
+                // while loop testing numInput with double.TryParse, out result assigned to cleanNum var
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("Invalid input. Please enter a whole number: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                // Get operator type from user
+                Console.WriteLine("Choose an operator from below:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.Write("Your option: ");
+                string op = Console.ReadLine();
+
+                // Try calculation, catch any errors
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in an error.\n");
+                    }
+                    else Console.WriteLine($"Your result is: {result}\n");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("ERROR--Details: " + e.Message);
+                }
+                Console.WriteLine("-------------------------\n");
+
+                // Ask the user if they want to keep using the app
+                Console.WriteLine("Would you like to continue using the app?\n");
+                Console.WriteLine("Type 'n' then Enter to close the app, or press any other button then Enter to continue\n");
+                if (Console.ReadLine() == "n") endApp = true;
+                Console.WriteLine("\n");               
+
             }
-            Console.Write("Press any key to close the Calculator app...");
-            Console.ReadKey();
-
-
         }
     }
 }
